2ab800d00086cb4b22cc4d94b53c85f87d451f9b
Doions <43123876+doions@users.noreply.github.com>
2024-03-23 18:42:37 +0530

    Update layout.js
    Footer Chnages.

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,9 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        © {new Date().getFullYear()}, Here is 
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <a href="https://www.lopu.in">Lopu</a>
       </footer>
     </div>
   )
